468139af2bacc7a1fd7889215b33875cc9ff8346
Sebastián Lastra <sebastian.lastra@usach.cl>
2013-07-16 02:01:49 -0400

    Entities created...

---
+++ src/Jmena/VentasBundle/Entity/Categoria.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Jmena\VentasBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Categoria
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Categoria
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Descripcion", type="string", length=255)
+     */
+    private $descripcion;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set descripcion
+     *
+     * @param string $descripcion
+     * @return Categoria
+     */
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+    
+        return $this;
+    }
+
+    /**
+     * Get descripcion
+     *
+     * @return string 
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+}
---
+++ src/Jmena/VentasBundle/Entity/Producto.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace Jmena\VentasBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Producto
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Producto
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Descripcion", type="string", length=255)
+     */
+    private $descripcion;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="Stock", type="integer")
+     */
+    private $stock;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="Valor", type="integer")
+     */
+    private $valor;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="Alarma", type="integer")
+     */
+    private $alarma;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Ruta_Imagen", type="string", length=255)
+     */
+    private $rutaImagen;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set descripcion
+     *
+     * @param string $descripcion
+     * @return Producto
+     */
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+    
+        return $this;
+    }
+
+    /**
+     * Get descripcion
+     *
+     * @return string 
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * Set stock
+     *
+     * @param integer $stock
+     * @return Producto
+     */
+    public function setStock($stock)
+    {
+        $this->stock = $stock;
+    
+        return $this;
+    }
+
+    /**
+     * Get stock
+     *
+     * @return integer 
+     */
+    public function getStock()
+    {
+        return $this->stock;
+    }
+
+    /**
+     * Set valor
+     *
+     * @param integer $valor
+     * @return Producto
+     */
+    public function setValor($valor)
+    {
+        $this->valor = $valor;
+    
+        return $this;
+    }
+
+    /**
+     * Get valor
+     *
+     * @return integer 
+     */
+    public function getValor()
+    {
+        return $this->valor;
+    }
+
+    /**
+     * Set alarma
+     *
+     * @param integer $alarma
+     * @return Producto
+     */
+    public function setAlarma($alarma)
+    {
+        $this->alarma = $alarma;
+    
+        return $this;
+    }
+
+    /**
+     * Get alarma
+     *
+     * @return integer 
+     */
+    public function getAlarma()
+    {
+        return $this->alarma;
+    }
+
+    /**
+     * Set rutaImagen
+     *
+     * @param string $rutaImagen
+     * @return Producto
+     */
+    public function setRutaImagen($rutaImagen)
+    {
+        $this->rutaImagen = $rutaImagen;
+    
+        return $this;
+    }
+
+    /**
+     * Get rutaImagen
+     *
+     * @return string 
+     */
+    public function getRutaImagen()
+    {
+        return $this->rutaImagen;
+    }
+}
